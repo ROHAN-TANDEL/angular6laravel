7dc6553e631f171052123aadb2e74cb55ade1955
rohan <rohan.t@scriptbees.com>
2018-06-29 15:50:49 +0530

    installing jquery in angular

--- src/app/ravon-home/ravon-home.component.ts
+++ src/app/ravon-home/ravon-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+//import * as $ from 'jquery';
+//import $ from "jquery";
+import $ = require('jquery')
 @Component({
   selector: 'app-ravon-home',
   templateUrl: './ravon-home.component.html',
@@ -10,6 +12,15 @@ export class RavonHomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	 $(document).ready(function(){
+       	$('.slider').slick({
+	    	slidesToShow: 3,
+	    	dots: true,
+	    	infinite: true,
+	    	cssEase: 'linear'
+		});
+    });
+
   }
 
 }
