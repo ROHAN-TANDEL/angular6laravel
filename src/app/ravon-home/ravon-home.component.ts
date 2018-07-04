64c8080867f9c60d6f203bdbd704c4f2c547adc8
rohan <rohan.t@scriptbees.com>
2018-07-04 13:58:40 +0530

    carousal completed added. backend process will be hte next step

--- src/app/ravon-home/ravon-home.component.ts
+++ src/app/ravon-home/ravon-home.component.ts
@@ -24,23 +24,37 @@ export class RavonHomeComponent implements OnInit {
 		  autoplay: true
 		});
 
-		 $('.slider-for').slick({
-		  slidesToShow: 1,
-		  slidesToScroll: 1,
-		  arrows: false,
-		  fade: true,
-		  asNavFor: '.slider-nav',
-			 autoplay: true
+		$('.slider-navi').slick({
+		  initialSlide: 0,
+		  slidesToShow: 6,
+		  slidesToScroll: 4,
+		  infinite: false,
+		  // arrows: true,
+		  fade: false,
+		  rtl:false,
+		  prevArrow: '<button type="button" class="slick-prev"><i class="material-icons">keyboard_arrow_left</i></button>',
+		  nextArrow: '<button type="button" class="slick-next"><i class="material-icons">keyboard_arrow_right</i></button>',
+		  lazyLoad:'progressive', //ondemand
+		  edgeFriction: 0.13,
+		  autoplay: false
+		});
+
+		$('.slider-for').slick({
+			slidesToShow: 5,
+			slidesToScroll: 5,
+			arrows: false,
+			fade: true,
+			asNavFor: '.slider-nav'
 		});
 		$('.slider-nav').slick({
-		  slidesToShow: 3,
-		  slidesToScroll: 1,
-		  asNavFor: '.slider-for',
-		  dots: false,
-		  centerMode: true,
-		  focusOnSelect: true
-
+			slidesToShow: 5,
+			slidesToScroll: 5,
+			asNavFor: '.slider-for',
+			dots: false,
+			centerMode: true,
+			focusOnSelect: true
 		});
+
 				
 
        	$('.mini-slider').slick({
@@ -54,7 +68,32 @@ export class RavonHomeComponent implements OnInit {
 		});
 
   	//main carousal END
-  	
+  	$('.center-data').slick({
+  centerMode: true,
+  centerPadding: '100px',
+  slidesToShow: 5,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
 	// $('.mini-slider-1').slick({
 	// 	slidesToShow: 1,
 	// 	slidesToScroll: 1,
