a3763f033eeb55eac9c55b17e219423b82a29bac
rohan <rohan.t@scriptbees.com>
2018-07-11 15:56:52 +0530

    update asset folder

--- services/data.service.ts
+++ services/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
   constructor(private http: HttpClient) { }
@@ -12,6 +13,7 @@ export class DataService {
   {
     return this.http.get('https://jsonplaceholder.typicode.com/users')
   }
+  
   getUser(userId)
   {
     return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
@@ -21,12 +23,46 @@ export class DataService {
   {
     return this.http.get('https://jsonplaceholder.typicode.com/posts')
   }
+  
   doChat()
   {
     return this.http.get('http://localhost:4203')
   }
+  
   getPost()
   {
     return this.http.get('http://localhost:4203')
+  }
+
+  Signup(email)
+  {
+//   const httpOptions = {
+//     headers: new HttpHeaders({ 
+//       'Access-Control-Allow-Origin':'http://localhost:4200','Access-Control-Allow-Headers':'*'})
+//   };
+// const postedData = {
+//   "client_id": "xcKBZqprT3GS1BjG8RBKgv7Py47f0XuY",
+//   "email": "rohan.t+7@scriptbees.com",
+//   "password": "Rohan@123",
+//   "connection": "Username-Password-Authentication",
+//   "user_metadata": { "plan": "silver", "enrollment_id": "340" }
+// }
+ var emailas    = 'email=56rohan@dd.com&';
+ var password = 'password=Rohan@123&';
+ var firstname =  'user_metadata[plans]=good&';
+ var enrollment_id = 'user_metadata[enrollment_id]=441';
+ let url      = 'https://krishananrohan.auth0.com/dbconnections/signup?'+emailas+password+'connection=Username-Password-Authentication'+firstname+enrollment_id;
+//     = {email: 'fa@fa.com', password: 'asdasd'};//{ email: this.getData.email, password: this.getData.password };
+      // return this.http.post('https://krishananrohan.auth0.com/dbconnections/signup', postedData, httpOptions).subscribe(result => {
+      //console.log(JSON.stringify(result));
+      return this.http.get(url);
+  }
+  Login() {
+
+
+let url  = 'https://krishananrohan.auth0.com/authorize?response_type=token&client_id=vYzQNhYdoqnCPBGMeAp407uMNLx594Xk&connection=Username-Password-Authentication&redirect_uri=http://localhost:4200&state=false';
+  return this.http.get(url);
+
+
   }
 }
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'services/data.service';
 import { Observable } from 'rxjs';
+//import { webAuth } from '';
 
 @Component({
   selector: 'app-posts',
@@ -16,6 +17,37 @@ export class PostsComponent implements OnInit {
     this.data.getPosts().subscribe(
       data => this.posts$ = data 
     );
+
+//     webAuth.authorize({
+//   //Any additional options can go here
+// });
+
+const array = [
+  { id: 333, email: 'rohan.t@gmail.com' },
+  { id: 334, email: 'rohan.t+1@gmail.com' },
+  { id: 335, email: 'rohan.t+2@gmail.com' },
+  { id: 336, email: 'rohan.t+3@gmail.com' },
+  { id: 337, email: 'rohan.t+4@gmail.com' },
+  { id: 338, email: 'rohan.t+5@gmail.com' },
+  { id: 339, email: 'rohan.t+6@gmail.com' },
+  { id: 340, email: 'rohan.t+7@gmail.com' }
+];
+  let count=0;
+  for(var num=0;num<array.length;num++) {
+   
+   if(array[num]['id'] == 336 && count==0) {
+     count=1;
+    console.log(array[num]['email']);
+    this.data.Login();
+    break;
+   }
+   else
+     count=0;
+}
+  return this.data.Login();
+
+    // this.data.Signup(array[num]['email']).subscribe(result => {        
+    //     console.log(result);
   }
 
 }
